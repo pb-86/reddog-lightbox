1ccc0138194c96886719473b237d3f63629f426f
Przemek Bąchorek <p.bachorek@gmail.com>
2021-04-20 13:46:32 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# RDDG Lightbox
---
+++ rddg-lightbox.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name: RDDG Lightbox
+ * Plugin URI: https://github.com/pb-86/rddg-Lightbox
+ * Description: Simple and lightweight plugin that provide lightbox gallery.
+ * Version: 0.1
+ * Author: Przemek Bąchorek
+ * Author URI: https://reddog.systems
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: rddglb
+ * Domain Path: /languages
+ *
+ * RDDG Lightbox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * RDDG Lightbox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with RDDG Lightobx.
+ *
+ * @package RDDG Lightbox
+ */
+
+/**
+ * Exit when accessed directly
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
