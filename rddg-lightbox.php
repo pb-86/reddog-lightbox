615d03ed45a460676ac98447896ac5fc9c9b188f
Przemek Bąchorek <p.bachorek@gmail.com>
2021-04-21 22:40:04 +0200

    0.2.5
    Added: more constants for better code organisation

--- rddg-lightbox.php
+++ rddg-lightbox.php
@@ -3,7 +3,7 @@
  * Plugin Name: RDDG Lightbox
  * Plugin URI: https://github.com/pb-86/rddg-Lightbox
  * Description: Simple and lightweight plugin that provide lightbox gallery.
- * Version: 0.2.4
+ * Version: 0.2.5
  * Author: Reddog Systems
  * Author URI: https://reddog.systems
  * License: GPLv2 or later
@@ -34,14 +34,17 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define( 'PLUGIN_VERSION', '0.2.4' );
+define( 'PLUGIN_VERSION', '0.2.5' );
 define( 'PLUGIN_PATH', plugins_url() . '/rddg-Lightbox/' );
+define( 'IMAGES_PATH', PLUGIN_PATH . 'assets/images/' );
+define( 'SCRIPTS_PATH', PLUGIN_PATH . 'assets/scripts/' );
+define( 'STYLES_PATH', PLUGIN_PATH . 'assets/styles/css/' );
 
 /**
  * Registering and enqueing JS files
  */
 function rddglb_add_scripts() {
-	wp_register_script( 'rddglb-master', PLUGIN_PATH . 'assets/scripts/master.js', array(), PLUGIN_VERSION, true );
+	wp_register_script( 'rddglb-master', SCRIPTS_PATH . 'master.js', array(), PLUGIN_VERSION, true );
 	wp_enqueue_script( 'rddglb-master' );
 }
 add_action( 'wp_enqueue_scripts', 'rddglb_add_scripts' );
@@ -50,7 +53,7 @@ add_action( 'wp_enqueue_scripts', 'rddglb_add_scripts' );
  * Registering and enqueing CSS files
  */
 function rddglb_add_styles() {
-	wp_register_style( 'rddglb-master', PLUGIN_PATH . 'assets/styles/css/master.css', array(), PLUGIN_VERSION, 'all' );
+	wp_register_style( 'rddglb-master', STYLES_PATH . 'master.css', array(), PLUGIN_VERSION, 'all' );
 	wp_enqueue_style( 'rddglb-master' );
 }
 add_action( 'wp_enqueue_scripts', 'rddglb_add_styles' );
@@ -75,8 +78,8 @@ add_filter( 'the_content', 'rddglb_add_modal', 50 );
 function rddglb_get_modal_html() {
 	$html  = '<div class="rddglb-modal" id="rddglb-modal" aria-hidden="true">';
 	$html .= '<div class="rddglb-modal__buttons">';
-	$html .= '<button class="rddglb-modal__button rddglb-modal__button--prev" id="rddglb-prevButton"><img src="' . plugins_url() . '/rddg-Lightbox/assets/images/icon-prev.svg" height="32" width="20"></button>';
-	$html .= '<button class="rddglb-modal__button rddglb-modal__button--next" id="rddglb-nextButton"><img src="' . plugins_url() . '/rddg-Lightbox/assets/images/icon-next.svg" height="32" width="20"></button>';
+	$html .= '<button class="rddglb-modal__button rddglb-modal__button--prev" id="rddglb-prevButton"><img src="' . IMAGES_PATH . 'icon-prev.svg" height="32" width="20"></button>';
+	$html .= '<button class="rddglb-modal__button rddglb-modal__button--next" id="rddglb-nextButton"><img src="' . IMAGES_PATH . 'icon-next.svg" height="32" width="20"></button>';
 	$html .= '</div>';
 	$html .= '<img class="rddglb-modal__full-image" id="rddglb-fullImage" src="">';
 	$html .= '</div>';
